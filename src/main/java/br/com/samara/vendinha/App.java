06f7fde749aafc2b8e813d94632181571021a8eb
Ana Carolina Moresco <anacmoresco1@gmail.com>
2021-07-27 11:48:01 -0300

    first commit

---
+++ src/main/java/br/com/samara/vendinha/App.java
@@ -0,0 +1,10 @@
+package br.com.samara.vendinha;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
